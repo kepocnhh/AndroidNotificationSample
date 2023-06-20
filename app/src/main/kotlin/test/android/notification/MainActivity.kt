6d57e390429bb7cdf46c368b3a2644e3670aa052
Stanley Wintergreen <kepocnhh@gmail.com>
2023-06-20 13:51:49 +0300

    init app

---
+++ app/src/main/kotlin/test/android/notification/MainActivity.kt
@@ -0,0 +1,10 @@
+package test.android.notification
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+internal class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
